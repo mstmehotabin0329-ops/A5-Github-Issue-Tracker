04497ec3e6cf8a19b7ab0e5232795c477e4a3376
mstmehotabin0329-ops <mstmehotabin0329@gmail.com>
2026-03-09 17:54:47 +0600

    Add login fuctionality and home page

---
+++ js/login.js
@@ -0,0 +1,18 @@
+const signInBtn = document.getElementById("signInBtn").addEventListener('click', function(){
+    const usernameInput = document.getElementById("input-username");
+    const username = usernameInput.value;
+    console.log(username);
+
+    const inputPassword = document.getElementById("password-input");
+    const password = inputPassword.value;
+    console.log(password);
+
+    if(username == "admin" && password =="admin123"){
+        alert('login Success');
+        window.location.assign("/home.html");
+    }
+    else{
+        alert("login Failed");
+        return;
+    }
+})
---
+++ js/script.js
@@ -0,0 +1,5 @@
+const loadingSpinner = document.getElementById("loading-spinner");
+const allBtn = document.getElementById("all-btn");
+const openBtn = document.getElementById("open-btn");
+const closedBtn = document.getElementById("closed-btn");
+const countProjectIssue = document.getElementById("countProjectIssue");
