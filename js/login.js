d50720724c0e146a28078740caa5dbeabe47b7d2
mstmehotabin0329-ops <mstmehotabin0329@gmail.com>
2026-03-12 12:09:16 +0600

    resolve login page issue

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@ const signInBtn = document.getElementById("signInBtn").addEventListener('click',
 
     if(username == "admin" && password =="admin123"){
         alert('login Success');
-        window.location.assign("/home.html");
+        window.location.assign("home.html");
     }
     else{
         alert("login Failed");
@@ -18,22 +18,4 @@ const signInBtn = document.getElementById("signInBtn").addEventListener('click',
 })
 
 
-
-
-// async function searchData() {
-//     const text = document.getElementById("search-issue");
-//     const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`);
-//     const data = await res.json()
-//     loadDisplay(data.data);
-// }
-
-
-// async function renderIssue(id) {
-//     const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
-//     const data = await res.json()
-//     if (issue == totalIssues) {
-//         alert(issue.title + "\n\n" + issue.description)
-//     }
-
-// }
 
